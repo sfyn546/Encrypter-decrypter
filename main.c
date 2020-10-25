2849eb761fdaf2d53ab3eb9e524c2c93695b5799
sfyn546 <54738747+sfyn546@users.noreply.github.com>
2020-10-25 13:35:20 +0530

    Update main.c

--- main.c
+++ main.c
@@ -3,22 +3,23 @@
 #include"enc.h"
 void main()
 {
-    char input[30],filename[20],tech[20];
+    char input[30],filename[20],tech[20],ch;
     int n,i,diff;
    printf("Welcome to Terminal:\n");
    printf("********************\n");
 
    while(1)
    {
-      
+      printf(">");
         scanf("%s",input);
         if(strcmp(input,"enc")==0)
         {
+             
             scanf("%s",filename);
+         
              scanf("%d",&diff);
-        
-        encrypti(filename,diff);
-            break;
+              encrypti(filename,diff);
+      
         }
        else if(strcmp(input,"create")==0)
        {
@@ -26,7 +27,7 @@ void main()
           scanf("%s",filename);
     
            create(filename);
-           break;
+      
        }
 
        else if(strcmp(input,"dec")==0)
@@ -35,14 +36,16 @@ void main()
             scanf("%d",&diff);
 
         decro(filename,diff);
-            break;
+  
         }
+               else if(strcmp(input,"exit")==0)
+               {
+                   break;
+               }
 
         else
         {
             printf("Command not found:%s\n",input);
         }
-
-        
 }
 }
