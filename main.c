b48cc1a69b85ce20c9ddce0873f37ed62eef92b5
sfyn546 <54738747+sfyn546@users.noreply.github.com>
2020-10-26 22:35:21 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,25 +1,77 @@
 #include<stdio.h>
+#include <stdlib.h>
 #include<string.h>
 #include"enc.h"
 void main()
 {
-    char input[30],filename[20],tech[20],ch;
-    int n,i,diff;
+    FILE  * fPtr;
+    char input[30],filename[20],tech[20],ch,logstr[100];
+    int n,i,diff,r;
    printf("Welcome to Terminal:\n");
    printf("********************\n");
+  
+  
 
    while(1)
    {
+
+       fPtr=fopen("log.txt","a");
+
+        
       printf("\n>");
         scanf("%s",input);
         if(strcmp(input,"enc")==0)
         {
              
             scanf("%s",filename);
-         
-             scanf("%d",&diff);
+
+            scanf("%s",tech);
+            scanf("%d",&diff);
+            if(strcmp(tech,"cypher")==0)
+            {
+                
               encrypti(filename,diff);
+              // logstr=str(filename)+"Encrypted By "+str(tech)"Technique"+"With a difference of "+str(diff);
+               
+              strcat(filename,"Encrypted by ");
+              strcat(filename,tech);
+              strcat(filename,"With a difference of ");
+             // strcat(filename,myd);
+
+                strcpy(logstr,filename);
+
+            }
+               
+            else if(strcmp(tech,"cypherAd")==0)
+            {
+                 scanf("%d",&r);
+                 cyphoAd(filename,diff,r);
+
+                 strcat(filename,"Encrypted by ");
+              strcat(filename,tech);
+              strcat(filename,"With a difference of ");
+                 // logstr=str(filename) + "Encrypted By "+str(tech)+ "Technique"+"With a difference of "+str(diff)+" and Reffect-"+str(r);
+            }
+         
+             else
+             {
+                 printf("No such command:%s %s %d",input,tech,diff);
+             }
+ 
       
+            
+        }
+
+
+        else if(strcmp(input,"del")==0)
+        {
+
+            scanf("%s",filename);
+             strcat(filename," Deleted\n");
+           strcpy(logstr,filename);
+           delete(filename);
+          
+
         }
        else if(strcmp(input,"create")==0)
        {
@@ -27,25 +79,49 @@ void main()
           scanf("%s",filename);
     
            create(filename);
+
+            strcat(filename," Created\n");
+           strcpy(logstr,filename);
+         
       
        }
 
        else if(strcmp(input,"dec")==0)
         {
-           scanf("%s",filename);
+             scanf("%s",filename);
+            
+            scanf("%s",tech);
             scanf("%d",&diff);
+            if(strcmp(tech,"cypher")==0)
+            {
+                
+              decro(filename,diff);
+             //   logstr=str(filename)+"Decrypted By "+str(tech)+ "Technique";      
+            }
 
-        decro(filename,diff);
+            else if(strcmp(tech,"cypherAd")==0)
+            {
+                 scanf("%d",&r);
+                decyphoAd(filename,diff,r);
+                 // logstr=str(filename)+"Decrypted By "+str(tech)+ "Technique"+"With a difference of "+str(diff)+" and Reffect-"+str(r);
+            }
+         
+             else
+             {
+                 printf("No such command:%s %s %d",input,tech,diff);
+             }
+
+
         }
 
             else if(strcmp(input,"write")==0)
                {
 
-                                                    scanf("%s",filename);
+                scanf("%s",filename);
     
-                                                writeInfile(filename);
+                 writeInfile(filename);
       
-                                  }
+              }
 
                else if(strcmp(input,"exit")==0||strcmp(input,"quit")==0)
                {
@@ -56,5 +132,14 @@ void main()
         {
             printf("Command not found:%s\n",input);
         }
+
+
+
+
+        fprintf(fPtr, "%s", logstr);
+         fclose(fPtr);
+  
 }
+
+ 
 }
